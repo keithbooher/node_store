a3371e3f19eef6ba04669cc384fc6a637e27c4f6
keithbooher <keith.booher@yahoo.com>
2020-05-24 22:44:40 -0500

    order page

--- client/src/components/admin/Orders/OrderPage.js
+++ client/src/components/admin/Orders/OrderPage.js
@@ -3,17 +3,19 @@ import { getOrder } from "../../../utils/API"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faEye, faEdit } from "@fortawesome/free-solid-svg-icons"
 import { Link } from "react-router-dom"
+import loadingGif from '../../../images/pizzaLoading.gif'
 
 class Orders extends Component {
   constructor(props) {
     super()
+    this.order_id = props.match.params.id
     this.state = {
-      order: props.order
+      order: null
     }
   }
   
   async componentDidMount() {
-    const order = await getOrder(this.order_id )
+    let order = await getOrder(this.order_id )
     order = order.data
     this.setState({ order })
   }
@@ -22,11 +24,17 @@ class Orders extends Component {
     let order = this.state.order
     return (
       <div>
-        <Link to="/admin/orders">Back Orders</Link>
-        <h3 className="underline">Order Data</h3>
-        <div>Order ID: {order._id}</div>
-        <h3 className="underline">Shipment Data</h3>
-          
+        {
+          this.state.order !== null ?
+            <>
+              <Link to="/admin/orders">Back Orders</Link>
+              <h3 className="underline">Order Data</h3>
+              <div>Order ID: {order._id}</div>
+              <h3 className="underline">Shipment Data</h3>
+            </>
+          : <img className="loadingGif" src={loadingGif} />
+        }
+
       </div>
     )
   }
