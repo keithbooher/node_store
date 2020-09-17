ae72b7d3e1ea0bcad23ec240fcf3e23b84e3fbb7
keithbooher <keith.booher@yahoo.com>
2020-09-16 22:42:59 -0500

    stripe replacement beginning

--- client/src/utils/API/billing.js
+++ client/src/utils/API/billing.js
@@ -3,8 +3,20 @@ import axios from "axios"
 import { ERROR } from '../../actions/types'
 import Bugsnag from '@bugsnag/js'
 
-export const handleToken = (token, amount) => async dispatch => {
-  let req = await axios.post('/api/stripe', { token, amount }).catch(error => {
+export const stripeIntent = (amount) => async dispatch => {
+  let req = await axios.post('/api/stripe/intent', { amount }).catch(error => {
+    dispatch({ type: ERROR, payload: error.response })
+    Bugsnag.notify(error)
+    return error.response
+  })
+  if (req.status === 200) {
+    return req
+  } else {
+    return {data: {}}
+  }
+}
+export const handleToken = (amount) => async dispatch => {
+  let req = await axios.post('/api/stripe/intent', { amount }).catch(error => {
     dispatch({ type: ERROR, payload: error.response })
     Bugsnag.notify(error)
     return error.response
