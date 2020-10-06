ce8e249bad64c1027aeb99212a93bfcfe5bf22f0
keithbooher <keith.booher@yahoo.com>
2020-10-06 16:01:33 -0500

    hopefully removing extraneaus scroll bars

--- client/src/components/customer_facing/components/Sidebar/Sidebar.js
+++ client/src/components/customer_facing/components/Sidebar/Sidebar.js
@@ -71,7 +71,7 @@ class Sidebar extends Component  {
     return (
       <>
         <div ref={node => this.node = node} className={"flex flex_column space-between theme-background-2 color-white sidebar " + sidebar_class}>
-          <div className="padding-m font-size-20  h-100 overflow-scroll">
+          <div className="padding-m font-size-20 h-100">
             <h3 className="margin-top-none">Categories</h3>
             {this.renderCategories(null)}
           </div>
