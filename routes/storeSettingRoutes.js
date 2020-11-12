7c0878f9c1bded21023f594c51bd351f8fc64f9e
keithbooher <keith.booher@yahoo.com>
2020-11-11 22:16:00 -0600

    set up to create tax setting

--- routes/storeSettingRoutes.js
+++ routes/storeSettingRoutes.js
@@ -68,22 +68,23 @@ module.exports = app => {
 
 
 
-//   // Create Store Setting
-//   app.get('/api/store_setting/create', async (req, res) => {  
-//     console.log("here")
-//     let setting = {
-//       name:"Show Gallery",
-//       value: {boolean: false},
-//       description:"Show gallery link in sidebar",
-//       internal_name: "show_gallery"
-//     }
-//     const storeSetting = new StoreSetting(setting)
-//     try {
-//       await storeSetting.save()
-//       res.send(storeSetting)
-//     } catch (err) {
-//       req.bugsnag.notify(err)
-//       res.status(422).send(err)
-//     }
-//   })
+  // Create Store Setting
+  app.get('/api/store_setting/create', async (req, res) => {  
+    console.log("here")
+    let setting = {
+      name:"Dont charge tax",
+      value: {boolean: false},
+      description:"Do not charge tax, only sell products at base price.",
+      internal_name: "no_tax"
+    }
+    const storeSetting = new StoreSetting(setting)
+    try {
+      await storeSetting.save()
+      res.send(storeSetting)
+    } catch (err) {
+      req.bugsnag.notify(err)
+      res.status(422).send(err)
+    }
+  })
+
 }
