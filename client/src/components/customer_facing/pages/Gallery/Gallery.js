75da8396c650241892c482fdc5452bade611c0b3
keithbooher <keith.booher@yahoo.com>
2020-11-25 14:49:13 -0600

    fix more linking

--- client/src/components/customer_facing/pages/Gallery/Gallery.js
+++ client/src/components/customer_facing/pages/Gallery/Gallery.js
@@ -27,11 +27,22 @@ async componentDidMount() {
         <div className="flex space-evenly flex-wrap" style={{ padding: "50px 0px" }}>
           {this.state.products ? 
             this.state.products.map((product, index) => {
+              let category_path_name
+              if (product.categories.length > 0) {
+                let cat = product.categories.find(cat => cat.deleted_at === undefined || cat.deleted_at === null)
+                if (cat) {
+                  category_path_name = cat.path_name
+                } else {
+                  category_path_name = "n_o_n_e"
+                }
+              } else {
+                category_path_name = "n_o_n_e"
+              }
               return (
                 <LazyLoadImage
                   src={product.images.i1}
                   wrapperClassName={`${this.props.mobile ? "gallery_card_image_mobile" : "gallery_card_image"} margin-s-v border-radius-s`}
-                  onClick={() => this.props.dispatchEnlargeImage({image: product.images.i1, path: "/shop/" + product.categories[0].path_name + "/" + product.path_name })}
+                  onClick={() => this.props.dispatchEnlargeImage({image: product.images.i1, path: "/shop/" + category_path_name + "/" + product.path_name })}
                 />
               )
             })
